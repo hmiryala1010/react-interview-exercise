84e092b341c07f70be47959b9f6cd6c23dd0484f
HASINI MIRYALA <160294217+hmiryala1010@users.noreply.github.com>
2025-06-14 01:15:59 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/react-interview-exercise/',
   plugins: [react()],
   resolve: {
     alias: {
